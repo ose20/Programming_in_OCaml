dfe5b046e97721d74dadb954d6099b9502f85747
ose20 <ayato96bell@gmail.com>
2020-12-20 21:59:15 +0900

    first commit

---
+++ chapter3/a.ml
@@ -0,0 +1,32 @@
+let pi = 3.1415926535
+
+let vod_cone =
+  let base = pi *. 2.0 *. 2.0 in
+  base *. 5.0 /. 3.0
+
+let cone_of_heightTwo r =
+  let base = r *. r *. pi in
+  base *. 2.0 /. 3.0
+
+let f x =
+  let x3 = x * x * x in
+  let x3_1 = x3 + 1 in
+  x3 + x3_1
+
+let g x =
+  let power3 x = x * x * x in
+  (power3 x) + (power3 (x + 1))
+
+let x = 2 and y = 1
+
+let x = y and y = x
+
+let z = 
+  let f x = "foo" ^ x
+  and y = 3.1 in
+  f (string_of_float y)
+
+
+let yen_of_dollar dollar =
+  let rate = 114.32 in
+  floor (rate *. dollar)
