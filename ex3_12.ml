009a3029a7c56426231952dbb943e51955bc86fe
ose20 <ayato96bell@gmail.com>
2021-01-03 15:34:46 +0900

    不要なファイルを削除しました

--- ex3_12.ml
+++
@@ -1,7 +0,0 @@
-let pos n =
-  let rec iter (i, res) =
-    if i < 0
-    then res
-    else iter (i - 1, res +. (1.0 /. (float_of_int (4 * i + 1))) -. (1.0 /. (float_of_int (4 * i + 3))))
-  in
-  iter (n, 0.0)
