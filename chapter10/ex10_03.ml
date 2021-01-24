aa927c0668bf37b0c9a19ea817e7a91ce0316c11
ose20 <ayato96bell@gmail.com>
2021-01-24 11:30:39 +0900

    changed type of the width of line from float to int

--- chapter10/ex10_03.ml
+++ chapter10/ex10_03.ml
@@ -6,17 +6,17 @@
   String と Bytes のライブラリをよしなに使う
   また、それらの 3 バイト文字の横幅は ASCII 文字の 2 倍と仮定する
 *)
-let limit = ref 80.0
+let limit = ref 80
 let filenames : string list ref = ref []
-let special_width = 2.0
+let special_width = 2
 
 let spec = [
-  ("-w", Arg.Int (fun i -> limit := (float_of_int i)), 
+  ("-w", Arg.Int (fun i -> limit := i), 
   "Specify a line width to use instead of the default 80 columns")
 ]
 
 let read_and_print fname =
-  let width = ref 0.0 in
+  let width = ref 0 in
   let in_ch = open_in fname in
   let buffer = Bytes.create 3 in
   Printf.printf "========== %s ==========\n" fname;
@@ -25,15 +25,15 @@ let read_and_print fname =
       let c = input_char in_ch in
       begin
         if c = '\n'
-        then (print_char '\n'; width := 0.0)
+        then (print_char '\n'; width := 0)
         else if int_of_char c <= 127
-        then (print_char c; width := !width +. 1.0)
+        then (print_char c; width := !width + 1)
         else
           (let c1 = input_char in_ch and c2 = input_char in_ch in
           Bytes.set buffer 0 c; Bytes.set buffer 1 c1; Bytes.set buffer 2 c2;
-          print_string (Bytes.to_string buffer); width := !width +. special_width)
+          print_string (Bytes.to_string buffer); width := !width + special_width)
       end;
-      if !width +. 0.9 >= !limit then (print_char '\n'; width := 0.0)
+      if !width >= !limit then (print_char '\n'; width := 0)
     done
   with End_of_file -> close_in in_ch
 
